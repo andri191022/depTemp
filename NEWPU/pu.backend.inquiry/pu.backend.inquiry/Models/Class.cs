ed59121b2bf904dc3fe376e1cc8882d361a62ca6
andri191022 <andri1022@gmail.com>
2024-10-21 15:47:47 +0700

    dfsd

---
+++ NEWPU/pu.backend.inquiry/pu.backend.inquiry/Models/Class.cs
@@ -0,0 +1,23 @@
+﻿namespace pu.backend.inquiry.Models
+{
+    public class ObjectHeader
+    {
+        public string UserName { get; set; }
+        public List<ObjectTransaction> objectTransactions { get; set; }
+
+    }
+
+    public class ObjectTransaction
+    {
+        public int Id { get; set; }
+        public ObjectInvoice Invoice { get; set; }
+    }
+
+    public class ObjectInvoice
+    {
+        public int IdInvoice { get; set; }
+
+    }
+}
+
+
--- NEWPU/pu.backend.inquiry/pu.backend.inquiry/Models/MX3_S_FormatRecModel.cs
+++ NEWPU/pu.backend.inquiry/pu.backend.inquiry/Models/MX3_S_FormatRecModel.cs
@@ -3,273 +3,266 @@ using System;
 using System.Collections.Generic;
 namespace pu.backend.inquiry.Models
 {
-
-    [XmlRoot(Namespace = "urn:iso:std:iso:20022:tech:xsd:pain.001.001.03")]
-    public class MX3_S_FormatRecModel
+    [XmlRoot(ElementName = "Document", Namespace = "urn:iso:std:iso:20022:tech:xsd:pain.001.001.03")]
+    public class MX3_S_Document
     {
-        [XmlElement("CstmrCdtTrfInitn")]
-        public CustomerCreditTransferInitiation CstmrCdtTrfInitn { get; set; }
+        [XmlElement(ElementName = "CstmrCdtTrfInitn")]
+        public MX3_S_CstmrCdtTrfInitn CstmrCdtTrfInitn { get; set; }
     }
 
-    public class CustomerCreditTransferInitiation
+    public class MX3_S_CstmrCdtTrfInitn
     {
-        [XmlElement("GrpHdr")]
-        public GroupHeader GrpHdr { get; set; }
+        [XmlElement(ElementName = "GrpHdr")]
+        public MX3_S_GrpHdr GrpHdr { get; set; }
 
-        [XmlElement("PmtInf")]
-        public PaymentInformation PmtInf { get; set; }
+        [XmlElement(ElementName = "PmtInf")]
+        public List<PmtInf> PmtInf { get; set; }
     }
 
-    public class GroupHeader
+    public class MX3_S_GrpHdr
     {
-        [XmlElement("MsgId")]
+        [XmlElement(ElementName = "MsgId")]
         public string MsgId { get; set; }
 
-        [XmlElement("CreDtTm")]
+        [XmlElement(ElementName = "CreDtTm")]
         public DateTime CreDtTm { get; set; }
 
-        [XmlElement("NbOfTxs")]
+        [XmlElement(ElementName = "NbOfTxs")]
         public int NbOfTxs { get; set; }
 
-        [XmlElement("InitgPty")]
-        public string InitgPty { get; set; } // Change to a proper class if needed
+        [XmlElement(ElementName = "InitgPty")]
+        public string InitgPty { get; set; } // Assuming this could be more complex, adjust as needed
     }
 
-    public class PaymentInformation
+    public class PmtInf
     {
-        [XmlElement("PmtInfId")]
+        [XmlElement(ElementName = "PmtInfId")]
         public string PmtInfId { get; set; }
 
-        [XmlElement("PmtMtd")]
+        [XmlElement(ElementName = "PmtMtd")]
         public string PmtMtd { get; set; }
 
-        [XmlElement("ReqdExctnDt")]
+        [XmlElement(ElementName = "ReqdExctnDt")]
         public DateTime ReqdExctnDt { get; set; }
 
-        [XmlElement("Dbtr")]
-        public string Dbtr { get; set; } // Change to a proper class if needed
+        [XmlElement(ElementName = "Dbtr")]
+        public string Dbtr { get; set; } // Assuming this could be more complex, adjust as needed
 
-        [XmlElement("DbtrAcct")]
-        public DebtorAccount DbtrAcct { get; set; }
+        [XmlElement(ElementName = "DbtrAcct")]
+        public MX3_S_DbtrAcct DbtrAcct { get; set; }
 
-        [XmlElement("DbtrAgt")]
-        public DebtorAgent DbtrAgt { get; set; }
+        [XmlElement(ElementName = "DbtrAgt")]
+        public MX3_S_DbtrAgt DbtrAgt { get; set; }
 
-        [XmlElement("CdtTrfTxInf")]
-        public List<CreditTransferTransactionInformation> CdtTrfTxInf { get; set; }
+        [XmlElement(ElementName = "CdtTrfTxInf")]
+        public List<MX3_S_CdtTrfTxInf> CdtTrfTxInf { get; set; }
     }
 
-    public class DebtorAccount
+    public class MX3_S_DbtrAcct
     {
-        [XmlElement("Id")]
-        public AccountId Id { get; set; }
+        [XmlElement(ElementName = "Id")]
+        public MX3_S_Id Id { get; set; }
     }
 
-    public class AccountId
+    public class MX3_S_Id
     {
-        [XmlElement("Othr")]
-        public Other Other { get; set; }
+        [XmlElement(ElementName = "Othr")]
+        public MX3_S_Othr Othr { get; set; }
     }
 
-    public class Other
+    public class MX3_S_Othr
     {
-        [XmlElement("Id")]
+        [XmlElement(ElementName = "Id")]
         public string Id { get; set; }
     }
 
-    public class DebtorAgent
+    public class MX3_S_DbtrAgt
     {
-        [XmlElement("FinInstnId")]
-        public string FinInstnId { get; set; } // Change to a proper class if needed
+        [XmlElement(ElementName = "FinInstnId")]
+        public string FinInstnId { get; set; } // Assuming this could be more complex, adjust as needed
     }
 
-    public class CreditTransferTransactionInformation
+    public class MX3_S_CdtTrfTxInf
     {
-        [XmlElement("PmtId")]
-        public PaymentId PmtId { get; set; }
+        [XmlElement(ElementName = "PmtId")]
+        public MX3_S_PmtId PmtId { get; set; }
 
-        [XmlElement("PmtTpInf")]
-        public PaymentTypeInformation PmtTpInf { get; set; }
+        [XmlElement(ElementName = "PmtTpInf")]
+        public MX3_S_PmtTpInf PmtTpInf { get; set; }
 
-        [XmlElement("Amt")]
-        public Amount Amt { get; set; }
+        [XmlElement(ElementName = "Amt")]
+        public MX3_S_Amt Amt { get; set; }
 
-        [XmlElement("ChrgBr")]
+        [XmlElement(ElementName = "ChrgBr")]
         public string ChrgBr { get; set; }
 
-        [XmlElement("CdtrAgt")]
-        public CreditorAgent CdtrAgt { get; set; }
+        [XmlElement(ElementName = "CdtrAgt")]
+        public MX3_S_CdtrAgt CdtrAgt { get; set; }
 
-        [XmlElement("Cdtr")]
-        public Creditor Cdtr { get; set; }
+        [XmlElement(ElementName = "Cdtr")]
+        public MX3_S_Cdtr Cdtr { get; set; }
 
-        [XmlElement("CdtrAcct")]
-        public CreditorAccount CdtrAcct { get; set; }
+        [XmlElement(ElementName = "CdtrAcct")]
+        public MX3_S_CdtrAcct CdtrAcct { get; set; }
 
-        [XmlElement("RgltryRptg")]
-        public List<RegulatoryReporting> RgltryRptg { get; set; }
+        [XmlElement(ElementName = "InstrForDbtrAgt")]
+        public string InstrForDbtrAgt { get; set; }
 
-        [XmlElement("RltdmRmtInf")]
-        public RelatedRemittanceInformation RltdmRmtInf { get; set; }
+        [XmlElement(ElementName = "RgltryRptg")]
+        public List<MX3_S_RgltryRptg> RgltryRptg { get; set; }
 
-        [XmlElement("RltdRmtInf")]
-        public List<RelatedRemittanceInformation> RltdRmtInf { get; set; }
+        [XmlElement(ElementName = "RltdRmtInf")]
+        public List<MX3_S_RltdRmtInf> RltdRmtInf { get; set; }
 
-        [XmlElement("RmtInf")]
-        public RemittanceInformation RmtInf { get; set; }
+        [XmlElement(ElementName = "RmtInf")]
+        public MX3_S_RmtInf RmtInf { get; set; }
     }
 
-    public class PaymentId
+    public class MX3_S_PmtId
     {
-        [XmlElement("InstrId")]
+        [XmlElement(ElementName = "InstrId")]
         public string InstrId { get; set; }
 
-        [XmlElement("EndToEndId")]
+        [XmlElement(ElementName = "EndToEndId")]
         public string EndToEndId { get; set; }
     }
 
-    public class PaymentTypeInformation
+    public class MX3_S_PmtTpInf
     {
-        [XmlElement("LclInstrm")]
-        public LocalInstrument LclInstrm { get; set; }
+        [XmlElement(ElementName = "LclInstrm")]
+        public MX3_S_LclInstrm LclInstrm { get; set; }
     }
 
-    public class LocalInstrument
+    public class MX3_S_LclInstrm
     {
-        [XmlElement("Prtry")]
+        [XmlElement(ElementName = "Prtry")]
         public string Prtry { get; set; }
     }
 
-    public class Amount
+    public class MX3_S_Amt
     {
-        [XmlElement("InstdAmt")]
-        public InstructedAmount InstdAmt { get; set; }
+        [XmlElement(ElementName = "InstdAmt")]
+        public MX3_S_InstdAmt InstdAmt { get; set; }
     }
 
-    public class InstructedAmount
+    public class MX3_S_InstdAmt
     {
-        [XmlAttribute("Ccy")]
-        public string Currency { get; set; }
+        [XmlAttribute(AttributeName = "Ccy")]
+        public string Ccy { get; set; }
 
         [XmlText]
-        public decimal AmountValue { get; set; }
+        public decimal Value { get; set; }
     }
 
-    public class CreditorAgent
+    public class MX3_S_CdtrAgt
     {
-        [XmlElement("FinInstnId")]
-        public FinancialInstitutionId FinInstnId { get; set; }
+        [XmlElement(ElementName = "FinInstnId")]
+        public MX3_S_FinInstnId FinInstnId { get; set; }
     }
 
-    public class FinancialInstitutionId
+    public class MX3_S_FinInstnId
     {
-        [XmlElement("Nm")]
-        public string Name { get; set; }
+        [XmlElement(ElementName = "Nm")]
+        public string Nm { get; set; }
+
+        [XmlElement(ElementName = "BIC")]
+        public string BIC { get; set; } // Include if needed
     }
 
-    public class Creditor
+    public class MX3_S_Cdtr
     {
-        [XmlElement("Nm")]
-        public string Name { get; set; }
+        [XmlElement(ElementName = "Nm")]
+        public string Nm { get; set; }
 
-        [XmlElement("PstlAdr")]
-        public PostalAddress PstlAdr { get; set; }
+        [XmlElement(ElementName = "PstlAdr")]
+        public MX3_S_PstlAdr PstlAdr { get; set; }
     }
 
-    public class PostalAddress
+    public class MX3_S_PstlAdr
     {
-        [XmlElement("TwnNm")]
-        public string TownName { get; set; }
+        [XmlElement(ElementName = "TwnNm")]
+        public string TwnNm { get; set; }
 
-        [XmlElement("Ctry")]
-        public string Country { get; set; }
+        [XmlElement(ElementName = "Ctry")]
+        public string Ctry { get; set; }
     }
 
-    public class CreditorAccount
+    public class MX3_S_RgltryRptg
     {
-        [XmlElement("Id")]
-        public AccountId Id { get; set; }
-    }
-
-    public class RegulatoryReporting
-    {
-        [XmlElement("DbtCdtRptgInd")]
+        [XmlElement(ElementName = "DbtCdtRptgInd")]
         public string DbtCdtRptgInd { get; set; }
 
-        [XmlElement("Authrty")]
-        public string Authrty { get; set; } // Change to a proper class if needed
+        [XmlElement(ElementName = "Authrty")]
+        public string Authrty { get; set; } // Assuming this could be more complex, adjust as needed
 
-        [XmlElement("Dtls")]
-        public RegulatoryDetails Dtls { get; set; }
+        [XmlElement(ElementName = "Dtls")]
+        public MX3_S_Dtls Dtls { get; set; }
     }
 
-    public class RegulatoryDetails
+    public class MX3_S_Dtls
     {
-        [XmlElement("Inf")]
+        [XmlElement(ElementName = "Inf")]
         public string Inf { get; set; }
     }
 
-    public class RelatedRemittanceInformation
+    public class MX3_S_RltdRmtInf
     {
-        [XmlElement("RmtLctnMtd")]
+        [XmlElement(ElementName = "RmtLctnMtd")]
         public string RmtLctnMtd { get; set; }
 
-        [XmlElement("RmtLctnElctrncAdr")]
+        [XmlElement(ElementName = "RmtLctnElctrncAdr")]
         public string RmtLctnElctrncAdr { get; set; }
     }
 
-    public class RemittanceInformation
+    public class MX3_S_RmtInf
     {
-        [XmlElement("Ustrd")]
+        [XmlElement(ElementName = "Ustrd")]
         public string Ustrd { get; set; }
 
-        [XmlElement("Strd")]
-        public StructuredRemittanceInformation Strd { get; set; }
+        [XmlElement(ElementName = "Strd")]
+        public MX3_S_Strd Strd { get; set; }
     }
 
-    public class StructuredRemittanceInformation
+    public class MX3_S_Strd
     {
-        [XmlElement("RfrdDocInf")]
-        public ReferredDocumentInformation RfrdDocInf { get; set; }
+        [XmlElement(ElementName = "RfrdDocInf")]
+        public MX3_S_RfrdDocInf RfrdDocInf { get; set; }
 
-        [XmlElement("RfrdDocAmt")]
-        public ReferredDocumentAmount RfrdDocAmt { get; set; }
+        [XmlElement(ElementName = "RfrdDocAmt")]
+        public MX3_S_RfrdDocAmt RfrdDocAmt { get; set; }
 
-        [XmlElement("CdtrRefInf")]
-        public CreditorReferenceInformation CdtrRefInf { get; set; }
+        [XmlElement(ElementName = "CdtrRefInf")]
+        public MX3_S_CdtrRefInf CdtrRefInf { get; set; }
 
-        [XmlElement("AddtlRmtInf")]
+        [XmlElement(ElementName = "AddtlRmtInf")]
         public string AddtlRmtInf { get; set; }
     }
 
-    public class ReferredDocumentInformation
+    public class MX3_S_RfrdDocInf
     {
-        [XmlElement("Nb")]
+        [XmlElement(ElementName = "Nb")]
         public string Nb { get; set; }
     }
 
-    public class ReferredDocumentAmount
+    public class MX3_S_RfrdDocAmt
     {
-        [XmlElement("RmtdAmt")]
-        public AmountAmount RmtdAmt { get; set; }
+        [XmlElement(ElementName = "RmtdAmt")]
+        public MX3_S_RmtdAmt RmtdAmt { get; set; }
     }
 
-    public class AmountAmount
+    public class MX3_S_RmtdAmt
     {
-        [XmlAttribute("Ccy")]
-        public string Currency { get; set; }
+        [XmlAttribute(AttributeName = "Ccy")]
+        public string Ccy { get; set; }
 
         [XmlText]
-        public decimal AmountValue { get; set; }
+        public decimal Value { get; set; }
     }
 
-    public class CreditorReferenceInformation
+    public class MX3_S_CdtrRefInf
     {
-        [XmlElement("Ref")]
+        [XmlElement(ElementName = "Ref")]
         public string Ref { get; set; }
     }
-
-
-
 
 }
